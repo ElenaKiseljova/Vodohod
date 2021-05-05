05bea3a03d30b3310f0eb48a349859052cd28689
Lena <e.a.kiseljova@gmail.com>
2021-05-05 11:56:32 +0300

    Create repository

---
+++ source/footer.php
@@ -0,0 +1,10 @@
+
+    <footer class="page-footer">
+      <div class="page-footer__wrapper">
+
+      </div>
+    </footer>
+
+    <script src="libs/swiper.min.js"></script>
+  </body>
+</html>
---
+++ source/index.php
@@ -0,0 +1,13 @@
+<?php
+  include("header.php");
+?>
+
+<main class="page-main">
+  <h1>
+    Заголовок
+  </h1>
+</main>
+
+<?php
+  include("footer.php");
+?>
