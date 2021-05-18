0af0d07df9b43c8e4ad902161d1dbad785e689fa
Lena <e.a.kiseljova@gmail.com>
2021-05-18 23:55:41 +0300

    еще поправила

--- source/js/promo.js
+++ source/js/promo.js
@@ -1,44 +1,38 @@
-// 'use strict';
-//
-// (function () {
-//   let changePromoHeight = function () {
-//     var orientation = window.orientation;
-//     let browserHeight = window.innerHeight;
-//
-//     let promo = document.querySelector('.promo');
-//     let promoWrapper = promo.querySelector('.promo__wrapper');
-//     let promoText = promo.querySelector('.text--promo');
-//
-//     if (orientation === 0) {
-//       if (promo && promoWrapper && promoText) {
-//         // Обнуляем стили
-//         promo.style = '';
-//
-//         let heightPromo = promo.offsetHeight;
-//
-//         // Обнуляем стили внешних отступов для текста
-//         promoText.style.marginBottom = '0px';
-//
-//         // Задаем новую высоту секции Промо
-//         promo.style.height = browserHeight + 'px';
-//
-//         // Растягиваем враппер Промо на всю его высоту
-//         promoWrapper.style.height = '100%';
-//       }
-//     } else {
-//       if (promo && promoWrapper && promoText) {
-//         // Обнуляем стили
-//         promoText.style = '';
-//         promo.style = '';
-//       }
-//     }
-//   };
-//
-//   document.addEventListener('DOMContentLoaded', function () {
-//     changePromoHeight();
-//   });
-//
-//   window.addEventListener('resize', function () {
-//     changePromoHeight();
-//   });
-// })();
+'use strict';
+
+(function () {
+  let changePromoHeight = function () {
+    var orientation = window.orientation;
+    let browserHeight = window.innerHeight;
+
+    let promo = document.querySelector('.promo');
+    let promoWrapper = promo.querySelector('.promo__wrapper');
+    let promoText = promo.querySelector('.text--promo');
+
+    if (promo && promoWrapper && promoText) {
+      // Обнуляем стили
+      promo.style = '';
+
+      let heightPromo = promo.offsetHeight;
+
+      if (heightPromo > browserHeight) {
+        promo.style.minHeight = browserHeight + 'px';
+      }
+
+      // // Обнуляем стили внешних отступов для текста
+      // promoText.style.marginBottom = '0px';
+      //
+      // // Задаем новую высоту секции Промо
+      // promo.style.height = browserHeight + 'px';
+      //
+      // // Растягиваем враппер Промо на всю его высоту
+      // promoWrapper.style.height = '100%';
+    }
+  };
+
+  changePromoHeight();
+
+  window.addEventListener('resize', function () {
+    changePromoHeight();
+  });
+})();
