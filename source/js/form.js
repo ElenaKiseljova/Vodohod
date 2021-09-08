654472bb41e5c553425959bee5048aee2bc5d71e
ElenaKiseljova <e.a.kiseljova@gmail.com>
2021-09-08 21:03:18 +0300

    - поправила валидацию форм

--- source/js/form.js
+++ source/js/form.js
@@ -8,9 +8,18 @@ document.addEventListener('DOMContentLoaded', () => {
       if (formCheck) {
         let buttonSubmit = formCheck.querySelector('button[type="submit"]');
 
+        // Сбрасываю, если атрибут прописан (чтобы клик прошел по кнопке)
+        buttonSubmit.disabled = false;
+        buttonSubmit.style.cursor = 'default';
+        buttonSubmit.querySelector('svg').style.fill = '#dcccb5';
+        buttonSubmit.querySelector('svg').style.pointerEvents = 'none';
+
         let inputEventListenerFlag = false;
 
-        let validateForm = function () {
+        /*
+          * field - text 'all' or element DOM
+        */
+        let validateForm = function (check = false, field = 'all') {
           // Validate flag
           let notValid = false;
 
@@ -29,20 +38,89 @@ document.addEventListener('DOMContentLoaded', () => {
                 if (!item.validity.valid) {
                   notValid = true;
 
-                  if (!item.closest('.invalid')) {
-                    // Попап формы
-                    if (item.closest('.form__row')) {
-                      item.closest('.form__row').classList.add('invalid');
-                      item.closest('.form__row').classList.remove('valid');
-                    }
+                  if (check && (field === 'all' || field === item)) {
+                    if (!item.closest('.invalid')) {
+                      // Попап формы
+                      if (item.closest('.form__row')) {
+                        item.closest('.form__row').classList.add('invalid');
+                        item.closest('.form__row').classList.remove('valid');
+                      }
 
-                    // Подписка
-                    if (item.closest('.subscribe__field')) {
-                      item.closest('.subscribe__field').classList.add('invalid');
-                      item.closest('.subscribe__field').classList.remove('valid');
+                      // Подписка
+                      if (item.closest('.subscribe__field')) {
+                        item.closest('.subscribe__field').classList.add('invalid');
+                        item.closest('.subscribe__field').classList.remove('valid');
+                      }
                     }
                   }
                 } else {
+                  if (check && (field === 'all' || field === item)) {
+                    if (item.closest('.invalid')) {
+                      // Попап формы
+                      if (item.closest('.form__row')) {
+                        item.closest('.form__row').classList.remove('invalid');
+                        item.closest('.form__row').classList.add('valid');
+                      }
+
+                      // Подписка
+                      if (item.closest('.subscribe__field')) {
+                        item.closest('.subscribe__field').classList.remove('invalid');
+                        item.closest('.subscribe__field').classList.add('valid');
+                      }
+                    } else if (!item.closest('.invalid') && !item.closest('.valid')) {
+                      // Попап формы
+                      if (item.closest('.form__row')) {
+                        item.closest('.form__row').classList.add('valid');
+                      }
+
+                      // Подписка
+                      if (item.closest('.subscribe__field')) {
+                        item.closest('.subscribe__field').classList.add('valid');
+                      }
+                    }
+                  }
+                }
+
+                if (inputEventListenerFlag === false) {
+                  item.addEventListener('blur', () => {
+                    validateForm(true, item);
+                  });
+
+                  let lastTimeout;
+                  item.addEventListener('input', () => {
+                    if (lastTimeout) {
+                      clearTimeout(lastTimeout);
+                    }
+
+                    lastTimeout = setTimeout(function () {
+                      validateForm(true, item);
+                    }, 500);
+                  });
+                }
+              }
+            });
+          }
+
+          if (allTextarea) {
+            allTextarea.forEach((item, i) => {
+              if (!item.validity.valid) {
+                notValid = true;
+
+                if (check && (field === 'all' || field === item)) {
+                  // Попап формы
+                  if (item.closest('.form__row')) {
+                    item.closest('.form__row').classList.add('invalid');
+                    item.closest('.form__row').classList.remove('valid');
+                  }
+
+                  // Подписка
+                  if (item.closest('.subscribe__field')) {
+                    item.closest('.subscribe__field').classList.add('invalid');
+                    item.closest('.subscribe__field').classList.remove('valid');
+                  }
+                }
+              } else {
+                if (check && (field === 'all' || field === item)) {
                   if (item.closest('.invalid')) {
                     // Попап формы
                     if (item.closest('.form__row')) {
@@ -55,53 +133,34 @@ document.addEventListener('DOMContentLoaded', () => {
                       item.closest('.subscribe__field').classList.remove('invalid');
                       item.closest('.subscribe__field').classList.add('valid');
                     }
-                  }
-                }
+                  } else if (!item.closest('.invalid') && !item.closest('.valid')) {
+                    // Попап формы
+                    if (item.closest('.form__row')) {
+                      item.closest('.form__row').classList.add('valid');
+                    }
 
-                if (inputEventListenerFlag === false) {
-                  item.addEventListener('input', () => {
-                    validateForm();
-                  });
-                }
-              }
-            });
-          }
-
-          if (allTextarea) {
-            allTextarea.forEach((item, i) => {
-              if (!item.validity.valid) {
-                notValid = true;
-
-                // Попап формы
-                if (item.closest('.form__row')) {
-                  item.closest('.form__row').classList.add('invalid');
-                  item.closest('.form__row').classList.remove('valid');
-                }
-
-                // Подписка
-                if (item.closest('.subscribe__field')) {
-                  item.closest('.subscribe__field').classList.add('invalid');
-                  item.closest('.subscribe__field').classList.remove('valid');
-                }
-              } else {
-                if (item.closest('.invalid')) {
-                  // Попап формы
-                  if (item.closest('.form__row')) {
-                    item.closest('.form__row').classList.remove('invalid');
-                    item.closest('.form__row').classList.add('valid');
-                  }
-
-                  // Подписка
-                  if (item.closest('.subscribe__field')) {
-                    item.closest('.subscribe__field').classList.remove('invalid');
-                    item.closest('.subscribe__field').classList.add('valid');
+                    // Подписка
+                    if (item.closest('.subscribe__field')) {
+                      item.closest('.subscribe__field').classList.add('valid');
+                    }
                   }
                 }
               }
 
               if (inputEventListenerFlag === false) {
+                item.addEventListener('blur', () => {
+                  validateForm(true, item);
+                });
+
+                let lastTimeout;
                 item.addEventListener('input', () => {
-                  validateForm();
+                  if (lastTimeout) {
+                    clearTimeout(lastTimeout);
+                  }
+
+                  lastTimeout = setTimeout(function () {
+                    validateForm(true, item);
+                  }, 500);
                 });
               }
             });
@@ -109,35 +168,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
           //console.log(notValid);
 
-          if (buttonSubmit) {
+          if (check) {
             if (!notValid) {
-              buttonSubmit.disabled = false;
+              buttonSubmit.style.cursor = 'pointer';
+              buttonSubmit.querySelector('svg').style.fill = '#c8a97a';
+              buttonSubmit.querySelector('svg').style.pointerEvents = 'all';
             } else {
-              buttonSubmit.disabled = true;
+              buttonSubmit.style.cursor = 'default';
+              buttonSubmit.querySelector('svg').style.fill = '#dcccb5';
+              buttonSubmit.querySelector('svg').style.pointerEvents = 'none';
             }
           }
 
           inputEventListenerFlag = true;
         };
 
-        formCheck.addEventListener('click', function (evt) {
+        // Запуск для добавления валидации при вводе в поля
+        validateForm();
+
+        buttonSubmit.addEventListener('click', function (evt) {
           //if (!evt.target.closest('.subscribe__field') && !evt.target.closest('.form__row')) {
-            validateForm();
+            validateForm(true, 'all');
           //}
         });
       }
     };
 
     // Order popup
-
     form('#order-popup');
 
     // Download contact
-
     form('#download-popup');
 
-    // subscribe
-
+    // Subscribe
     form('#subscribe-form');
   } catch (e) {
     console.log(e);
